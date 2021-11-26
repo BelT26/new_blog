bca40fe7cf69a91b11412fce118840e9ec61685c
BelT26 <helen.taylor@hotmail.it>
2021-11-26 17:11:16 +0000

    add styling

--- my_site/blog/urls.py
+++ my_site/blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("posts", views.posts, name="posts"),
-    path("post", views.post, name="post")
+    path("<int:selected_post>", views.post_by_number),
+    path("<str:selected_post>", views.post, name="my_post")
 ]
--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import  HttpResponseNotFound, HttpResponseRedirect, Http404
-
+from django.http import HttpResponseNotFound, HttpResponseRedirect, Http404, HttpResponse
+from django.urls import reverse
+from datetime import date
 
 post_dict = {
     'one': 'good morning',
@@ -10,34 +11,104 @@ post_dict = {
     'five': 'goodbye'
 }
 
+post_dicts = [
+    {
+        'slug': 'hike-in-the-moutains',
+        'author': 'helen',
+        'date': date(2021, 11, 26),
+        'image': 'mountains.jpg',
+        'title': 'Hiking in the Mountains',
+        'excerpt': 'I love hiking in the mountains',
+        'content': """
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                 Doloremque unde ad sint accusantium veritatis laborum ex,
+                 facilis eos sequi! Beatae, asperiores? Error nisi nam optio
+                 assumenda quasi aperiam sint ullam? """
+    },
+    {
+        "slug": "programming-is-fun",
+        "image": "coding.jpg",
+        "author": "Maximilian",
+        "date": date(2022, 3, 10),
+        "title": "Programming Is Great!",
+        "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
+        "content": """
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+        """
+    },
+    {
+        "slug": "into-the-woods",
+        "image": "woods.jpg",
+        "author": "Maximilian",
+        "date": date(2020, 8, 5),
+        "title": "Nature At Its Best",
+        "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
+        "content": """
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+        """
+    }
+]
+
+def get_date(post):
+    return post['date']
 
 # Create your views here.
 def index(request):
     """ returns homepage """
-    post_list = list(post_dict.keys())
-    return render(request, 'blog/index.html')
+    sorted_posts = sorted(post_dicts, key=get_date)
+    latest_posts = sorted_posts[-3:]
+    return render(request, 'blog/index.html', {
+        'posts': latest_posts
+    })
 
 
 def posts(request):
     """ returns summary of all posts """
-    post_list = list(post_dict.keys())
     return render(request, 'blog/posts.html', {
-        'posts': post_list
+        'all_posts': post_dicts
     })
 
 
-def post(request):
-    """ returns an individual post """
-    return render(request, 'blog/post.html')
+def post_by_number(request, selected_post):
+    post_list = list(post_dict.keys())
     
+    if selected_post > len(post_list):
+        return HttpResponseNotFound('Sorry there aren\'t that many posts!') 
 
-def monthly_challenge(request, month):
+    post_num = post_list[selected_post-1]
+    redirect_path = reverse("post", args=[post_num])
+    return HttpResponseRedirect(redirect_path)
+
+
+
+def post(request, selected_post):
     try:
-        challenge_text = monthly_challenges[month]
-        return render(request, "challenges/challenge.html", {
-            'text': challenge_text,
-            'month': month
+        text = post_dict[selected_post]
+        return render(request, "blog/post.html", {
+            'text': text,
+            'post': selected_post
         })
     except:
         raise Http404()
+
   
